fee8459fae19cd8e92b5c29dd0e8fff4edd3bdc0
Jens Pfahl <jens.pfahl@mailbox.org>
2024-02-27 20:46:16 +0100

    Continue tracking when app restart during tracking

--- lib/service/LocalNotificationService.dart
+++ lib/service/LocalNotificationService.dart
@@ -181,7 +181,7 @@ class LocalNotificationService {
     await _flutterLocalNotificationsPlugin.cancelAll();
   }
 
-  void handleAppLaunchNotification() {
+  void handleAppLaunchNotification(Function (List<int>) handleActiveNotificationPostHandler) {
     _flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails()
         .then((notification) {
           final payload = notification?.notificationResponse?.payload;
@@ -199,7 +199,7 @@ class LocalNotificationService {
       activeNotifications.forEach((element) {
         _handleActiveNotification(element.id, element.channelId);
       });
-
+      handleActiveNotificationPostHandler(activeNotifications.map((e) => e.id).toList());
     });
   }
 
